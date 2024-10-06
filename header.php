123a7a0a372581977e4b3a1a1db361fbe73ff4c6
Nehemiah Cabico <ncabico.a12137232@umak.edu.ph>
2024-10-06 16:10:56 +0800

    Header.php has been changed

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <head>
 		<meta charset="UTF-8">
         <title>Ezra</title>
+        <p> Welcome to our website </p>
         <link type="text/css" rel="stylesheet" href	="chomedesign.css">
     </head>
 
